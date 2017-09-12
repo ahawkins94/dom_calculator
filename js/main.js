15221da19da8f5332a8706e36c5c336b96174d08
ahawkins94 <adamhawkins94@hotmail.co.uk>
2017-09-12 23:51:32 +0100

    can't do any more. Confused as to why I can't get num1 to even log to the browser's console let alone save a variable to it. Disappointed with how much I achieved.

--- js/main.js
+++ js/main.js
@@ -2,29 +2,33 @@ document.addEventListener('DOMContentLoaded', function (event) {
 
 	console.log('The DOM is ready!!!');
 
-	var numbers = document.getElementsByClassName("number")
-	var operators = document.getElementsByClassName("operator")
+	var numbers = document.getElementsByClassName("number");
+	var operators = document.getElementsByClassName("operator");
 
-	var num1 = 	(function getNumbers() {
-					for (i = 0; i < numbers.length; i++) {
-						numbers[i].addEventListener('click', function (event){
-							return event.target.value;
-						})
-					}
-				})
+	var num1 = 0;
+
+	function getNumbers() {
+		for (i = 0; i < numbers.length; i++) {
+			numbers[i].addEventListener('click', function (event) {
+				if (num1 = 0) {
+					num1 = event.target.value;
+					console.log(num1);
+			}
+		})
+	}
 
 
 
 	function getOperators() {
 		for (i = 0; i < operators.length; i++) {
 			operators[i].addEventListener('click', function (event){
-				console.log(event.target.value);
-			})
+				})
+			}
 		}
 
 	}
 
-getOperators();
+getNumbers();
 
 
 	// number.addEventListener('click', function (event) {
