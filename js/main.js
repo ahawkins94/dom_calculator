0ed85c36f90353fc01f3f9f6ed9c79c154c19c83
ahawkins94 <adamhawkins94@hotmail.co.uk>
2017-09-12 22:18:41 +0100

    just spent 3 hours slamming my head into a brick wall because i didn't call a function good banter

--- js/main.js
+++ js/main.js
@@ -2,14 +2,24 @@ document.addEventListener('DOMContentLoaded', function (event) {
 
 	console.log('The DOM is ready!!!');
 
-	var number = document.getElementsByClassName('number');
-	var operator = document.getElementsByClassName('operator')
+	var number = document.getElementsByClassName("number")
+
+
+	function getNumbers() {
+		for (i = 0; i < number.length; i++) {
+			number[i].addEventListener('click', function (event){
+				console.log('clicked')
+			})
+		}
+
+	}
+getNumbers();
 
 	console.log(number)
 
-	number.addEventListener('click', function (event) {
-		console.log('help me, Ive been clicked!')
-	})
+	// number.addEventListener('click', function (event) {
+	// 	console.log('help me, Ive been clicked!')
+	// })
 
 
 });
